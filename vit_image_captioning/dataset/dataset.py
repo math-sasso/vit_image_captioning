6bb56f27637e03a6da803149a57b930e32c2a569
Matheus Gustavo Alves Sasso <matheus.sasso17@gmail.com>
2021-11-24 14:22:09 -0300

    Update dataset.py

--- vit_image_captioning/dataset/dataset.py
+++ vit_image_captioning/dataset/dataset.py
@@ -1,3 +1,12 @@
+import torch
+import numpy as np
+import pandas as pd
+from PIL import Image
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+from torchvision import transforms
+from torch.utils.data import Dataset
+
 class MyDataset(Dataset):
     """Pytorch dataset para treinamento da rede
 
@@ -10,23 +19,28 @@ class MyDataset(Dataset):
         captions_per_image: número de legendas por imagem
     """
 
-    def __init__(self,h5_images_path: str,json_captions_path: str,tokenizer,
-                 max_tokens_captions,transforms=None, captions_per_image=5):
-        self.h5_images_path = h5_images_path
-        self.json_captions_path = json_captions_path
-        
-        self.caption_data = json.load(open(json_captions_path, 'r'))
-        self.transforms = transforms
+    def __init__(self,images_path: str, df_captions: pd.DataFrame,tokenizer,
+                 max_tokens_captions, img_size, captions_per_image=5):
+        self.images_path = images_path
+        self.df_captions = df_captions
         self.tokenizer = tokenizer
         self.max_tokens_captions = max_tokens_captions
         self.captions_per_image = captions_per_image
+        self.img_size = img_size
+        #import pdb;pdb.set_trace()
     
     def __len__(self):
-        return len(self.caption_data)
+        return len(self.df_captions)
 
     def __transforms(self,image):
+        transfors_func =  transforms.Compose([
+        transforms.ToPILImage(),
+        create_transform(**resolve_data_config(
+            {},model='vit_base_patch16_224'))
+        ])
+        return transfors_func(image)
 
-    def _tokenize(self, input_text):
+    def __tokenize(self, input_text):
         tokenized = self.tokenizer(
             input_text, return_tensors='pt',padding='max_length',
             max_length=self.max_tokens_captions,pad_to_max_length=True,
@@ -34,54 +48,32 @@ class MyDataset(Dataset):
             add_special_tokens=True
             )
         return tokenized['input_ids'].squeeze()
+      
+    def __tensorize_image(self,img):
+        img_tensor = torch.from_numpy(img).to(torch.uint8) # (H, W, C)
+        return img_tensor.transpose(0,2).transpose(1,2) # (C, H, W)
 
     def __getitem__(self, idx):
-        raw_captions = self.caption_data[idx]
-        
-        image_data = h5py.File(self.h5_images_path, 'r')['images']
-        
-        num_captions = len(raw_captions)
-        assert num_captions == self.captions_per_image
 
-        # Random choice of one out of 5 captions
-        chosen_target_caption = raw_captions[random.choice(range(num_captions))]
-        tokenized_target_caption = self._tokenize(chosen_target_caption)
+        # Get dataframe idx
+        batch_captions = self.df_captions.loc[idx]
         
-        image = torch.from_numpy(image_data[idx]).to(torch.uint8) # (C, H, W)
-        transformed_image = image
-        
-        if self.transforms is not None:
-            transformed_image = self.transforms(image)
-        
+        # Retrieve image
+        img = Image.open(f"{self.images_path}/{batch_captions['img_id']}.jpg")
+        img = img.resize((self.img_size,self.img_size), Image.ANTIALIAS)
+        image_data = np.asarray( img )
+        image_tensor = self.__tensorize_image(image_data)
+        transformed_image_tensor = self.__transforms(image_tensor)
+            
+        # Tokenize Caption
+        target_caption = batch_captions["caption"]
+        tokenized_target_caption = self.__tokenize(target_caption)
+
 
         rets = {
-            'original_image': image,
-            'transformed_image': transformed_image,
-            'raw_captions': raw_captions,
-            'tokenized_target_caption': tokenized_target_caption
-            }
-        return rets
-    
-    @staticmethod
-    def collate_fn(batch):
-        original_image = torch.stack(
-            [item['original_image']
-             for item in batch], dim=0)
-        
-        transformed_image = torch.stack(
-            [item['transformed_image'] for item in batch], dim=0)
-        
-        # List of lists: (batch_size * captions_per_image)
-        raw_captions = [item['raw_captions'] for item in batch]
-        # raw_captions = list(zip(*[item['raw_captions'] for item in batch]))
-
-        tokenized_target_caption = torch.stack(
-            [item['tokenized_target_caption'] for item in batch], dim=0)
-        
-        rets = {
-            'original_image': original_image,
-            'transformed_image': transformed_image,
-            'raw_captions': raw_captions,
+            'image_tensor': image_tensor,
+            'transformed_image_tensor': transformed_image_tensor,
+            'target_caption': target_caption,
             'tokenized_target_caption': tokenized_target_caption
             }
         return rets
